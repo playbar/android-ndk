754adcc71dd6406a79ab2e4b53503287e96bd62a
hgl888 <hgl868@126.com>
2016-10-09 18:07:33 +0800

    modify code

--- gles3jni/app/src/main/java/com/android/gles3jni/GLES3JNILib.java
+++ gles3jni/app/src/main/java/com/android/gles3jni/GLES3JNILib.java
@@ -20,11 +20,11 @@ package com.android.gles3jni;
 
 public class GLES3JNILib {
 
-     static {
-          System.loadLibrary("gles3jni");
-     }
-
-     public static native void init();
-     public static native void resize(int width, int height);
-     public static native void step();
+//     static {
+//          System.loadLibrary("gles3jni");
+//     }
+//
+//     public static native void init();
+//     public static native void resize(int width, int height);
+//     public static native void step();
 }
--- gles3jni/app/src/main/java/com/android/gles3jni/GLES3JNIView.java
+++ gles3jni/app/src/main/java/com/android/gles3jni/GLES3JNIView.java
@@ -38,22 +38,41 @@ class GLES3JNIView extends GLSurfaceView {
         super(context);
         // Pick an EGLConfig with RGB8 color, 16-bit depth, no stencil,
         // supporting OpenGL ES 2.0 or later backwards-compatible versions.
-        setEGLConfigChooser(8, 8, 8, 0, 16, 0);
-        setEGLContextClientVersion(2);
+//        setEGLContextFactory(new ContextFactory());
+//        setEGLConfigChooser(8, 8, 8, 0, 16, 0);
+        setEGLConfigChooser( 5, 6, 5, 0, 0, 0);
+        setEGLContextClientVersion(3);
         setRenderer(new Renderer());
+    }
+
+    private static class ContextFactory implements GLSurfaceView.EGLContextFactory {
+        private static int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
+        public EGLContext createContext(EGL10 egl, EGLDisplay display, EGLConfig eglConfig) {
+            Log.w(TAG, "creating OpenGL ES 2.0 context");
+            int[] attrib_list = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE };
+            EGLContext context = egl.eglCreateContext(display, eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list);
+            return context;
+        }
+
+        public void destroyContext(EGL10 egl, EGLDisplay display, EGLContext context) {
+            egl.eglDestroyContext(display, context);
+        }
     }
 
     private static class Renderer implements GLSurfaceView.Renderer {
         public void onDrawFrame(GL10 gl) {
-            GLES3JNILib.step();
+            Log.e("GLES3JNIView", "onDrawFrame");
+//            GLES3JNILib.step();
         }
 
         public void onSurfaceChanged(GL10 gl, int width, int height) {
-            GLES3JNILib.resize(width, height);
+            Log.e("GLES3JNIView", "onSurfaceChanged");
+//            GLES3JNILib.resize(width, height);
         }
 
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            GLES3JNILib.init();
+            Log.e("GLES3JNIView", "onSurfaceCreated");
+//            GLES3JNILib.init();
         }
     }
 }
