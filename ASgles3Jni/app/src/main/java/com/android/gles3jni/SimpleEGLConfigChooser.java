044f90075f32b3fabf13645d2b5aa75d58d507a1
playbar <hgl868@126.com>
2017-08-18 11:46:23 +0800

    删除 gles 工程

--- ASgles3Jni/app/src/main/java/com/android/gles3jni/SimpleEGLConfigChooser.java
+++
@@ -1,8 +0,0 @@
-package com.android.gles3jni;
-
-public class SimpleEGLConfigChooser extends ComponentSizeChooser {
-    public SimpleEGLConfigChooser(boolean withDepthBuffer) {
-        super(8, 8, 8, 0, withDepthBuffer ? 16 : 0, 0);
-    }
-}
-
